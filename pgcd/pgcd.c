fb6319e23197b32abdc25b2b785f3c041eecf732
Madirex <angel@madirex.com>
2024-07-23 14:40:35 +0200

    added pgcd

---
+++ pgcd/pgcd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    if (argc == 3)
+    {
+        int n1 = atoi(argv[1]);
+        int n2 = atoi(argv[2]);
+
+        if (n1 > 0 && n2 > 0)
+        {
+            while (n1 != n2)
+            {
+                if (n1 > n2)
+                    n1 -= n2;
+                else
+                    n2 -= n1;
+            }
+            printf("%d", n1);
+        }
+    }
+    printf("\n");
+    return (0);
+}
